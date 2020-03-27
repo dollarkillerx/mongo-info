95fe93d70e59ca9112c7bdcabd8f1cf0d808c0d1
dollarkiller <adapawang@gmail.com>
2020-03-27 15:13:39 +0800

    add

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -8,6 +8,7 @@ package examples
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
@@ -153,5 +154,133 @@ func TestFindAll3(t *testing.T) {
 	if err != nil {
 		log.Fatalln("S1: ",err)
 	}
+	log.Println("OK")
+}
+
+
+// 解码一些奇怪的东西
+type A struct {
+	Name string
+	Data []c
+}
+
+type c interface {
+	_a()
+}
+
+type b1 struct {
+	Name string
+	Pc string
+}
+func (b *b1) _a() {}
+
+type b2 struct {
+	Name string
+	Age int
+}
+func (b *b2) _a() {}
+
+func TestInAndOut(t *testing.T) {
+	a := A{
+		Name:"你好",
+		Data:[]c{
+			&b1{
+				Name:"b1",
+				Pc:"P2",
+			},
+			&b2{
+				Name:"N2",
+				Age:18,
+			},
+		},
+	}
+
+	db,err := New("mongodb://localhost:27017", "test")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	collection := db.Collection("colection1")
+	insert, err := collection.Insert(&a)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(insert)
+
+	result, err := collection.Select(bson.M{"name": "你好"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b := &A{}
+	err = result.Decode(b)
+	if err != nil {
+		//log.Fatalln("1", err)
+	}
+	log.Println(b)
+
+
+
+	marshal, err := json.Marshal(a)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(marshal))
+
+	bb := &A{}
+	err = json.Unmarshal(marshal, bb)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(bb)
+}
+
+
+func TestInterface(t *testing.T) {
+	type s1 struct {
+		Name string
+		Ad interface{}
+	}
+	type a struct {
+		Age int
+	}
+
+	// 如果是interface 将无法解析
+	db,err := New("mongodb://localhost:27017", "test")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	collection := db.Collection("colection1")
+
+	b := s1{
+		Name:"1112",
+		Ad:&a{
+			Age:16,
+		},
+	}
+
+	insert, err := collection.Insert(b)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("OK: ",insert)
+
+	result, err := collection.Select(bson.M{"name": "1112"})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c := &s1{}
+	err = result.Decode(c)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	_,ok := c.Ad.(*a)
+	fmt.Println(c.Ad)
+	if !ok {
+		log.Fatalln("Eeeee")
+	}
+
 	log.Println("OK")
 }
